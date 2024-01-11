27d51b8c1b14ed51fc5194498a0a208c4f18604c
rahmadmukti <rahmad.zonemail@gmail.com>
2024-01-11 10:24:04 +0700

    fixing midtrans snap redirect and callback

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
 use App\Http\Requests\User\Checkout\Store;
-use App\Models\Camp;
 use App\Mail\Checkout\AfterCheckout;
+use App\Models\Camp;
+use App\Models\Discount;
 use Auth;
 use Mail;
 use Str;
@@ -15,14 +16,14 @@ use Midtrans;
 
 class CheckoutController extends Controller
 {
-    function __construct()
-    {
-        Midtrans\Config::$serverKey = env('MIDTRANS_SERVERKEY');
-        Midtrans\Config::$isProduction = env('MIDTRANS_IS_PRODUCTION');
-        Midtrans\Config::$isSanitized = env('MIDTRANS_IS_SANITIZED');
-        Midtrans\Config::$is3ds = env('MIDTRANS_IS_3DS');
-    }
 
+    public function __construct()
+    {
+        Midtrans\Config::$serverKey     = env('MIDTRANS_SERVERKEY');
+        Midtrans\Config::$isProduction  = env('MIDTRANS_IS_PRODUCTION');
+        Midtrans\Config::$isSanitized   = env('MIDTRANS_IS_SANITIZED');
+        Midtrans\Config::$is3ds         = env('MIDTRANS_IS_3DS');
+    }
 
     /**
      * Display a listing of the resource.
@@ -46,7 +47,7 @@ class CheckoutController extends Controller
             return redirect(route('user.dashboard'));
         }
         return view('checkout.create', [
-            'camp'  => $camp
+            'camp' => $camp
         ]);
     }
 
@@ -58,22 +59,25 @@ class CheckoutController extends Controller
      */
     public function store(Store $request, Camp $camp)
     {
-        //Mapping Request Data
+        // mapping request data
         $data = $request->all();
-        $data['user_id']    = Auth::id();
-        $data['camp_id']    = $camp->id;
+        $data['user_id'] = Auth::id();
+        $data['camp_id'] = $camp->id;
 
-        //Update User Data
-        $user               = Auth::user();
-        $user->email        = $data['email'];
-        $user->name         = $data['name'];
-        $user->occupation   = $data['occupation'];
+        // update user data
+        $user = Auth::user();
+        $user->email      = $data['email'];
+        $user->name       = $data['name'];
+        $user->occupation = $data['occupation'];
+        $user->phone      = $data['phone'];
+        $user->address    = $data['address'];
         $user->save();
 
-        //Create Checkout
-        $checkout           = Checkout::create($data);
+        // create checkout
+        $checkout = Checkout::create($data);
         $this->getSnapRedirect($checkout);
-        //Send E-mail
+
+        // sending email
         Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
 
         return redirect(route('checkout.success'));
@@ -124,7 +128,6 @@ class CheckoutController extends Controller
         //
     }
 
-
     public function success()
     {
         return view('checkout.success');
@@ -135,8 +138,9 @@ class CheckoutController extends Controller
      */
     public function getSnapRedirect(Checkout $checkout)
     {
-        $orderId = $checkout->id . '-' . Str::random(5);
-        $price   = $checkout->Camp->price * 1000;
+        $orderId    = $checkout->id . '-' . Str::random(5);
+        $price      = $checkout->Camp->price * 1000;
+
         $checkout->midtrans_booking_code = $orderId;
 
         $transaction_details = [
@@ -144,14 +148,14 @@ class CheckoutController extends Controller
             'gross_amount'          => $price,
         ];
 
-        $item_details = [
+        $item_details[] = [
             'id'                    => $orderId,
             'price'                 => $price,
             'quantity'              => 1,
-            'name'                  => "Payment for {$checkout->Camp->title} Camp",
+            'name'                  => "Payment for {$checkout->Camp->title} Camp"
         ];
 
-        $userData = [
+        $userData[] = [
             'first_name'            => $checkout->User->name,
             'last_name'             => "",
             'address'               => $checkout->User->address,
@@ -162,13 +166,12 @@ class CheckoutController extends Controller
         ];
 
         $customer_details = [
-            'first_name'            => $checkout->User->name,
-            'last_name'             => "",
-            'email'                 => $checkout->User->email,
-            'phone'                 => $checkout->User->phone,
-            'billing_address'       => $userData,
-            'shipping_address'      => $userData,
-
+            "first_name"            => $checkout->User->name,
+            "last_name"             => "",
+            "email"                 => $checkout->User->email,
+            "phone"                 => $checkout->User->phone,
+            "billing_address"       => $userData,
+            "shipping_address"      => $userData,
         ];
 
         $midtrans_params = [
@@ -178,8 +181,8 @@ class CheckoutController extends Controller
         ];
 
         try {
-            //Get Snap URL
-            $paymentUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+            // Get Snap Payment Page URL
+            $paymentUrl = \Midtrans\Snap::createTransaction($midtrans_params)->redirect_url;
             $checkout->midtrans_url = $paymentUrl;
             $checkout->save();
 
@@ -189,9 +192,9 @@ class CheckoutController extends Controller
         }
     }
 
-    function midtransCallback(Request $request)
+    public function midtransCallback(Request $request)
     {
-        $notif = new Midtrans\Notification();
+        $notif = $request->method() == 'POST' ? new Midtrans\Notification() : Midtrans\Transaction::status($request->order_id);
 
         $transaction_status = $notif->transaction_status;
         $fraud = $notif->fraud_status;
